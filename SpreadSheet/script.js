f747f8bc2666d83ce58dffaa0257c11065e8a1ae
meanzi3 <minjichoe0326@gmail.com>
2024-02-23 11:49:15 +0900

    update - 1
    focus 된 셀의 위쪽 헤더와 오른쪽 헤더가 함께 하늘 색으로 하이라이트 되도록 구현

--- SpreadSheet/script.js
+++ SpreadSheet/script.js
@@ -9,12 +9,23 @@ const spreadsheet = [];
 const alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 
 class Cell {
-  constructor(isHeader, disabled, data, row, column, active = false) {
+  constructor(
+    isHeader,
+    disabled,
+    data,
+    row,
+    column,
+    rowName,
+    columnName,
+    active = false
+  ) {
     this.isHeader = isHeader;
     this.disabled = disabled;
     this.data = data;
     this.row = row;
     this.column = column;
+    this.rowName = rowName;
+    this.columnName = columnName;
     this.active = active;
   }
 }
@@ -28,24 +39,39 @@ function initSpreadsheet() {
     for (let j = 0; j < COLS; j++) {
       let cellData = "";
       let isHeader = false;
+      let disabled = false;
 
       // 1 ~ 9 헤더 설정
       if (j == 0) {
         cellData = i;
         isHeader = true;
+        disabled = true;
       }
 
       // A ~ I 헤더 설정
       if (i == 0) {
         cellData = alphabets[j - 1];
         isHeader = true;
+        disabled = true;
       }
 
       if (!cellData) {
         cellData = "";
       }
 
-      const cell = new Cell(isHeader, false, cellData, i, j, false);
+      const rowName = i;
+      const columnName = alphabets[j - 1];
+
+      const cell = new Cell(
+        isHeader,
+        disabled,
+        cellData,
+        i,
+        j,
+        rowName,
+        columnName,
+        false
+      );
       spreadsheetRow.push(cell);
     }
     spreadsheet.push(spreadsheetRow);
@@ -54,6 +80,7 @@ function initSpreadsheet() {
   console.log(spreadsheet);
 }
 
+// cell elements 생성
 function createCellEl(cell) {
   const cellEl = document.createElement("input");
   cellEl.className = "cell";
@@ -64,7 +91,36 @@ function createCellEl(cell) {
   if (cell.isHeader) {
     cellEl.classList.add("header");
   }
+
+  cellEl.onclick = () => handleCellClick(cell);
   return cellEl;
+}
+
+// 셀 클릭 이벤트 함수
+function handleCellClick(cell) {
+  // 이전에 클릭한 셀의 헤더 active 먼저 지우기
+  clearHeaderActiveStates();
+
+  // 클릭한 셀의 column, row 정보 가져오기
+  const columnHeader = spreadsheet[0][cell.column];
+  const rowHeader = spreadsheet[cell.row][0];
+
+  // 요소 가져오기
+  const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
+  const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
+
+  // active 주기
+  columnHeaderEl.classList.add("active");
+  rowHeaderEl.classList.add("active");
+}
+
+function getElFromRowCol(row, col) {
+  return document.querySelector("#cell_" + row + col);
+}
+
+function clearHeaderActiveStates() {
+  const headers = document.querySelectorAll(".header.active");
+  headers.forEach((header) => header.classList.remove("active"));
 }
 
 function drawSheet() {
